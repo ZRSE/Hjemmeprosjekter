47762eaba903bb26a371b2a10ef69f9ce567702c
ZRSE <Zar.r96@gmail.com>
2019-05-15 12:50:32 +0200

    Testprosjeker

---
+++ TestProsjekt/src/Main/TestMain.java
@@ -0,0 +1,34 @@
+package Main;
+
+public class TestMain {
+	
+	public static int numberX = 0;
+	public static int numberY = 1;
+	public static int numberZ;
+
+	
+	public static void FibonacciNumbers() {
+	
+		for(int i = 2; i < 20; i++) {
+			
+			
+			numberZ = numberX+numberY;
+			
+			System.out.print(" " + numberZ);
+			
+			numberX = numberY;
+			numberY = numberZ;
+		}
+
+	}
+	
+	
+	public static void main(String[] args) {
+			
+			System.out.print(numberX +" "+ numberY);
+			FibonacciNumbers();
+			
+		}
+	
+
+}
